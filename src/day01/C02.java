7fd7a3de151d2a8a5fe728a592269c77b731a051
esertkaya <sertkayavolkan@gmail.com>
2023-04-28 11:09:24 +0200

    C02'de aciklamalar yazildi

--- src/day01/C02.java
+++ src/day01/C02.java
@@ -5,9 +5,18 @@ public class C02 {
     //Bu komut her proje icin sadece bir kere calistirilir
     //Bu komut ile birlikte git icinde bulunan klasoru kontrol etmeye baslar
     //Bu klasorde her yedek almak istedigimizde commit yapmaliyiz
+
     //Asagidaki 3 komut bizim dosyalarimizi her yedeklemek istedigimizde kullanilir
     // 1- git add. : Son kaydedilen versiyondan sonra projede yapilan degisiklikleri belirler
-    // 2-
+    // 2- git commit -m "commit ismi" : Yazdigimiz projenin son durumunu bir version olarak kaydeder
+          //Burada " " icinde yazilan commit isminin anlamli olmasinda fayda vardir
+          // Ilerleyen asamalarda geri donus yapmak istersek hangi commit'e donmek isteyecegimizi
+          //aciklamalardan bulabiliriz
+
+    //git ile ilisiklendirdigimiz dosyadaki class isimleri git durumuna gore renklenir
+    //kirmizi : bu class herhangi bir version'a eklenmedi
+    //mavi    : bu dosya vir version'a eklendi ama sonradan degisiklik yapilmis
+    //siyah   : bu class bir version'da son haliyle yedeklendi, sonrasinda hic bir degisiklik yok
 
 
 }
